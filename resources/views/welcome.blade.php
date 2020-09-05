25cd8da908de5cc80a8879f1db88517ca4b8364b
Mohamed <webdeveloper.int99@gmail.com>
2020-09-05 04:26:29 +0200

    project movie app first version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-    <div class="h-screen w-full container-fluid">
-        <img src="/images/moviebg.jpg"  class="h-full w-screen object-fill">
+    <div class="h-screen w-full flex justify-center items-center bg-gray-600">
+        <h1 class="text-gray-100 font-bold" style="font-size:40px">Welcome To Our Movie App</h1>
     </div>
 @endsection
